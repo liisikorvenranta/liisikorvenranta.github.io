7c26b332cdb6c66d7028f8dd4f7bef088516a3ee
liisikorvenranta <liisi.korvenranta@aalto.fi>
2017-05-03 12:32:02 +0300

    uloskommentointi

--- animation2.js
+++ animation2.js
@@ -1,5 +1,7 @@
 var game = new Phaser.Game(600, 400, Phaser.CANVAS, 'ydinvoima', { preload: preload, create: create });
 
+var xcord = 200;
+
 function preload() {
 
     //  You can fill the preloader with as many assets as your game requires
@@ -10,8 +12,8 @@ function preload() {
     //  The second parameter is the URL of the image (relative)
     game.load.image('iceberg', 'iceberg.png');
     game.load.image('coal', 'coal.png');
-    game.load.image('bear1','jaakarhu3.png');
-    game.load.image('bear2','jaakarhu4.png');
+    game.load.image('bear1','jaakarhu3.png'); //vasen
+    game.load.image('bear2','jaakarhu4.png'); //oikea
     game.load.image('atom','atom.png');
 
 }
@@ -32,8 +34,27 @@ function create() {
     a.height = 35;
     c.width = 35;
     c.height = 35;
-    var j = game.add.image(200,300,'bear1');
+    var j = game.add.image(xcord,300,'bear1');
 	game.physics.enable(j, Phaser.Physics.ARCADE);
 	j.body.velocity.x=150;
+
+}
+
+function update() {
+    
+    j.body.velocity.x = 0;
+    j.body.velocity.y = 0;
+
+    if (cursors.left.isDown)
+    {
+        j.body.velocity.x = -200;
+        j.scale.x = 1;
+    }
+    else if (cursors.right.isDown)
+    {
+        j = 'bear2';
+        j.body.velocity.x = 200;
+        j.scale.x = -1;
+    }
 
 }
