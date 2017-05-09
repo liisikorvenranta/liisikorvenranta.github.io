7384b536a7e81dbf6d8f2f7af79a995198dcfba5
annischalin <anni.schalin@gmail.com>
2017-05-09 14:00:26 +0300

    vika
    vika

--- states/gametitle.js
+++ states/gametitle.js
@@ -19,11 +19,11 @@ gameTitle.prototype = {
 		playButton.anchor.setTo(0.5,0.5);
         
         var a = game.add.sprite(300, 200, 'atom'); //images
-		var a2 = game.add.sprite(590,480,'atom');
+		var a2 = game.add.sprite(570,480,'atom');
 		a2.height = 40;
 		a2.width = 40;
-        game.add.sprite(230, 430, 'ohje');
-        var c = game.add.sprite(550, 450, 'coal');   
+        game.add.sprite(210, 430, 'ohje');
+        var c = game.add.sprite(530, 450, 'coal');   
         c.height = 40;
         c.width = 40;
 	},
